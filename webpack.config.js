e4e3b69685b099d637fc497402e7a29da428d7da
UK-taniyama <uk.taniyama@gmail.com>
2016-12-31 14:50:02 +0900

    riotjs-loader->custom-loader(riot)

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,24 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const extractHTML = new ExtractTextPlugin('[name].html');
 const stringify = require('webpack-stringify-loaders');
+const customLoader = require('custom-loader');
+const riot = require('riot-compiler');
+const utils = require('loader-utils');
+
+customLoader.loaders.riot = function(source) {
+  const query = utils.parseQuery(this.query);
+  return riot.compile(source, query.options || {});
+};
+customLoader.loaders.dummy = function(source) {
+  "version,context,request,query,data,cacheable,loaders,loaderIndex,resource,resourcePath,resourceQuery,value,inputValue,options,debug,minimize,sourceMap,target,webpack".split(",").forEach((name) => {
+    console.log(name, this[name]);
+  });
+  "ext,name,path,folder".split(",").forEach((name) => {
+    var value = utils.interpolateName(this, `[${name}]`, {context: this.options.context});
+    console.log(name, value);
+  });
+  return source;
+};
 
 module.exports = {
   entry: {
@@ -18,7 +36,10 @@ module.exports = {
       test: /\.tag\.pug$/,
       exclude: /node_modules/,
       loader: stringify([{
-        loader: 'riotjs',
+        loader: 'custom',
+        query: {
+          name: 'riot',
+        },
       }, {
         loader: 'pug-html',
         query: {
